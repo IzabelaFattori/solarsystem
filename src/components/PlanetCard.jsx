83bbbb5efd8a93591a63ffa587bbe209f3584489
IzabelaFattori <izabela.fattori@hotmail.com>
2022-10-21 15:53:04 -0300

    6 quase pronto

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import planets from '../data/planets';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <p>
+        <p data-testid="planet-name">
           {' '}
           { planetName }
           {' '}
         </p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <ul className="planetas">
+          {planets.map((planet) => (
+            <li key={ planet.name }>
+              { planet.name }
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
