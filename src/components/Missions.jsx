9fba40c3d7b40a5ac139918fc088b91433ea9d90
IzabelaFattori <izabela.fattori@hotmail.com>
2022-10-21 16:05:18 -0300

    continuando

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
 
 export default class Missions extends Component {
   render() {
+    const { missionName, missionImage } = this.props;
     return (
-      <div data-testid="missions">Missions</div>
+      <div data-testid="missions">
+        <Title headline={ missionName } />
+      </div>
     );
   }
 }
