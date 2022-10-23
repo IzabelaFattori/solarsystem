4da53e6141f4974a1e4f41366af81ff1fbae1ee3
IzabelaFattori <izabela.fattori@hotmail.com>
2022-10-23 16:00:12 -0300

    subindo projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <p>Sistema Solar</p>
         <Header />
         <SolarSystem headline="Planetas" />
         <Missions headline="Missões" />
